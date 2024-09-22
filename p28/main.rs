3b21787de5257be5760c91cdb95dbee5daad3982
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-09-22 17:50:22 -0500

    msg

--- p28/main.rs
+++ p28/main.rs
@@ -1,38 +1,9 @@
 fn main() {
-    let mut spf: Vec<i32> = vec![1; MAX_N];
-    sieve(&mut spf);
-
-    // program
+	println!("{}", number_spiral_diagonal_sum(1001));
 }
 
-const MAX_N: usize = 10_000_001;
+fn number_spiral_diagonal_sum(dims: i32) -> i32 {
+	let mut sum = 0;
 
-fn sieve(spf: &mut Vec<i32>) {
-	spf[0] = 0;
-	spf[1] = 1;
-    spf[2] = 2;
-
-    for i in (4..MAX_N).step_by(2) {
-        spf[i] = 2;
-    }
-
-	for i in (3..(MAX_N as i32)).step_by(2) {
-		if spf[i as usize] == 1 {
-			for j in (i..(MAX_N as i32)).step_by(i as usize) {
-				spf[j as usize] = i;
-			}
-		}
-	}
-}
-
-fn sieve_factorization(mut x: i32, spf: &Vec<i32>) -> Vec<i32> {
-	let mut ret = Vec::new();
-
-	while x != 1 {
-		ret.insert(0, spf[x as usize]);
-
-		x /= spf[x as usize];
-	}
-
-    ret
+	sum
 }
