c16d11ddfc711c7cb765dc26f75bf42c5ddbf2f2
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-09-22 23:07:10 -0500

    finished problem 28 using a brute force solution

--- p28/main.rs
+++ p28/main.rs
@@ -3,7 +3,24 @@ fn main() {
 }
 
 fn number_spiral_diagonal_sum(dims: i32) -> i32 {
-	let mut sum = 0;
+	let mut sum = 1;
+
+	let mut separation = 2;
+	let mut iter = 0;
+
+	let mut pos = 1;
+
+	while pos < dims * dims {
+		pos += separation;
+		iter += 1;
+
+		if iter == 4 {
+			separation += 2;
+			iter = 0;
+		}
+
+		sum += pos;
+	}
 
 	sum
 }
